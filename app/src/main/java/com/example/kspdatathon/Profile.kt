c2030ae571e05b18644787ea1c466da4c5ad0ae0
JudeSamJ <judesamuelsjj@gmail.com>
2024-03-18 19:29:44 +0530

    success

---
+++ app/src/main/java/com/example/kspdatathon/Profile.kt
@@ -0,0 +1,26 @@
+package com.example.kspdatathon
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+class Profile:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.profile)
+
+        val profileButton: ImageButton = findViewById(R.id.profile_button)
+        profileButton.setOnClickListener {
+            val intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)
+
+        }
+        val logoutButton: Button = findViewById(R.id.logout_button)
+        logoutButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
--- app/src/main/java/com/example/kspdatathon/chatbot.kt
+++ app/src/main/java/com/example/kspdatathon/chatbot.kt
@@ -1,11 +1,21 @@
 package com.example.kspdatathon
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
-class ChatActivity : AppCompatActivity(){
+class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.chatbot)
+        val chatButton: ImageButton = findViewById(R.id.chat_arrow)
+        chatButton.setOnClickListener {
+            val intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
+
 }
