56df750a57494b8857023d25a2d21775d878c452
JudeSamJ <judesamuelsjj@gmail.com>
2024-03-21 09:47:29 +0530

    success

---
+++ app/src/main/java/com/example/kspdatathon/Signup.kt
@@ -0,0 +1,25 @@
+package com.example.kspdatathon
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class Signup:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.signup)
+
+        val signupLogin: Button = findViewById(R.id.signup_login)
+        signupLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+
+    }
+        val signup: Button = findViewById(R.id.login_button)
+        signup.setOnClickListener {
+            val intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)
+
+}}}
--- app/src/main/java/com/example/kspdatathon/login_activity.kt
+++ app/src/main/java/com/example/kspdatathon/login_activity.kt
@@ -1,10 +1,12 @@
 package com.example.kspdatathon
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,33 @@ class LoginActivity : AppCompatActivity() {
         myButton.setOnClickListener {
             val intent = Intent(this, DashboardActivity::class.java)
             startActivity(intent)
+        }
+        val policeButton: ImageButton = findViewById(R.id.policeit_button)
+        policeButton.setOnClickListener {
+            val intent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://ksp.karnataka.gov.in/new-page/POLICE%20IT/en")
+            )
+            startActivity(intent)
+
+        }
+        val googleButton: ImageButton = findViewById(R.id.google_button)
+        googleButton.setOnClickListener {
+            val intent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://hack2skill.com/login")
+            )
+            startActivity(intent)
+
+        }
+        val xButton: ImageButton = findViewById(R.id.x_button)
+        xButton.setOnClickListener {
+            val intent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://twitter.com/kspfactcheck")
+            )
+            startActivity(intent)
+
         }
     }
 }
