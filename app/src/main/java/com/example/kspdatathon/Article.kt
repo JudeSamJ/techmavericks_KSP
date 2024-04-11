edaf6d18dbde38572b2d48cd9a34bbb5aec3b59a
JudeSamJ <judesamuelsjj@gmail.com>
2024-04-11 14:10:32 +0530

    success

--- app/src/main/java/com/example/kspdatathon/Article.kt
+++ app/src/main/java/com/example/kspdatathon/Article.kt
@@ -1,25 +1,59 @@
 package com.example.kspdatathon
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.net.Uri
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Toast
+import android.Manifest
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.app.ActivityCompat
+import com.example.kspdatathon.databinding.Article1Binding
+import java.io.File
 
-class Article:AppCompatActivity() {
+class Article : AppCompatActivity() {
+    private lateinit var binding: Article1Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.article1)
+        binding = Article1Binding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.uploadButton.setOnClickListener { showFileChooser() }
+
         val articleButton: ImageButton = findViewById(R.id.back_button)
         articleButton.setOnClickListener {
             val intent = Intent(this, DashboardActivity::class.java)
             startActivity(intent)
         }
-        val uploadButton: Button = findViewById(R.id.upload_button)
-        uploadButton.setOnClickListener {
-            val intent = Intent(this, Article_1::class.java)
-            startActivity(intent)
 
+
+    }
+    private fun showFileChooser(){
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type="*/*"
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        try {
+            startActivityForResult(Intent.createChooser(intent,"Select a file"),
+                100)
+        } catch (exception:Exception){
+            Toast.makeText(this,"Please install a file manager.",Toast.LENGTH_SHORT).show()
         }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode==100 && resultCode== RESULT_OK && data != null){
+            val uri: Uri? = data.data
+            val path: String=uri?.path.toString()
+            val file = File(path)
+            binding.pathText.text = "Path: $path File name: ${file.name}".trimIndent()
+    }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
