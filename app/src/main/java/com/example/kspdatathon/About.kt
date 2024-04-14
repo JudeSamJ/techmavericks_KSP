1a7f240f4030fddc43ecf9de2bfa39e585435208
JudeSamJ <judesamuelsjj@gmail.com>
2024-04-14 19:11:34 +0530

    success

--- app/src/main/java/com/example/kspdatathon/About.kt
+++ app/src/main/java/com/example/kspdatathon/About.kt
@@ -1,19 +1,22 @@
 package com.example.kspdatathon
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Process // Import for Process.killProcess
+import android.widget.ImageButton
 
 class About : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_about)
-
-        val quitButton = findViewById<Button>(R.id.sosbutton)
-        quitButton.setOnClickListener {
-            Process.killProcess(Process.myPid())
+        val profileButton: ImageButton = findViewById(R.id.imageButton)
+        profileButton.setOnClickListener {
+            val intent = Intent(this, Settings::class.java)
+            startActivity(intent)
+        }
         }
     }
-}
+
